929d68bc22bc0062ef3b72439e6f440ae45f0d5f
Kvitlich <Kvitlich@gmail.com>
2020-06-11 15:31:58 +0600

    Last

--- VirtualMemory_Demonstrator/Form1.cs
+++ VirtualMemory_Demonstrator/Form1.cs
@@ -20,17 +20,17 @@ namespace VirtualMemory_Demonstrator
         private System.Threading.Timer timer;
         private List<Array> arrays = new List<Array>();
         public enum AllocatingMode
-        { 
-            MB = 1, 
-            KB = 2, 
-            Byte = 3, 
-        }      
+        {
+            MB = 1,
+            KB = 2,
+            Byte = 3,
+        }
 
         public enum FillMode
         {
             Full = 1,
             OnEnds = 2,
-            NoFill = 3, 
+            NoFill = 3,
             FiftyP = 4,
         }
 
@@ -56,15 +56,17 @@ namespace VirtualMemory_Demonstrator
 
         private void Alloc(object sender, EventArgs e)
         {
+
+
             button_alloc.Enabled = false;
             int value;
-            if (!Int32.TryParse(alloc_value.Text,out value))
+            if (!Int32.TryParse(alloc_value.Text, out value))
             {
                 MessageBox.Show("Поле должно быть числом!");
                 button_alloc.Enabled = true;
                 return;
             }
-            switch((AllocatingMode)comboBoxMode.SelectedItem)
+            switch ((AllocatingMode)comboBoxMode.SelectedItem)
             {
                 case AllocatingMode.MB:
                     value *= 1024 * 1024;
@@ -76,32 +78,40 @@ namespace VirtualMemory_Demonstrator
                     break;
             }
             value /= 2;
-            char[] temp = new char[value];
-
-            switch ((FillMode)comboBoxFill.SelectedItem)
+            try
             {
-                case FillMode.Full:
-                    for (int i = 0; i < temp.Length; i++)
-                    {
-                        temp[i] = ')';
-                    }
-                    break;
-                case FillMode.FiftyP:
-                    for (int i = 0; i < temp.Length/2; i++)
-                    {
-                        temp[i] = ')';
-                    }
-                    break;
-                case FillMode.NoFill:
-                    break;
+                char[] temp = null;
+                temp = new char[value];
 
-                case FillMode.OnEnds:
-                    temp[0] = '0';
-                    temp[temp.Length - 1] = '0';
-                    break;
+                switch ((FillMode)comboBoxFill.SelectedItem)
+                {
+                    case FillMode.Full:
+                        for (int i = 0; i < temp.Length; i++)
+                        {
+                            temp[i] = ')';
+                        }
+                        break;
+                    case FillMode.FiftyP:
+                        for (int i = 0; i < temp.Length / 2; i++)
+                        {
+                            temp[i] = ')';
+                        }
+                        break;
+                    case FillMode.NoFill:
+                        break;
+
+                    case FillMode.OnEnds:
+                        temp[0] = '0';
+                        temp[temp.Length - 1] = '0';
+                        break;
+                }
+
+                arrays.Add(temp);
             }
-
-            arrays.Add(temp);
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
             button_free.Enabled = true;
             button_alloc.Enabled = true;
         }
@@ -111,12 +121,20 @@ namespace VirtualMemory_Demonstrator
             alloc_value.Enabled = true;
             button_alloc.Enabled = true;
             button_free.Enabled = false;
-            for (int i = 0; i < arrays.Count; i++)
+            try
             {
-                arrays[i] = null;
+                for (int i = 0; i < arrays.Count; i++)
+                {
+                    arrays[i] = null;
+                }
+                arrays.Clear();
+
+                GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
             }
-            arrays.Clear();
-            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         public delegate void InvokeDelegate(object param);
@@ -124,7 +142,7 @@ namespace VirtualMemory_Demonstrator
         {
             cb = new TimerCallback(ShowInfo);
             timer = new System.Threading.Timer(cb, null, 0, 2000);
-   
+
         }
 
         public void ShowInfo(object obj)
